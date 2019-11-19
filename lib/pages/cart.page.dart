354b19e3719a57c3f5d5e61a0ee706c31ae936ec
talesboalim <talesboalim@gmail.com>
2019-11-18 23:57:15 -0300

    criada tela do carrinho

--- lib/pages/cart.page.dart
+++ lib/pages/cart.page.dart
@@ -9,8 +9,7 @@ class CartPage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Container(
-              color: Colors.red,
-              child: Text("Container 1"),
+              child: productList(),
             ),
           ),
           Container(
@@ -22,4 +21,101 @@ class CartPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget productList() {
+  return ListView(
+    children: <Widget>[
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      // productItem(context, "BeoPlay Speaker", "755", "assert/product-1.png"),
+      // productItem(context, "Leather", "450", "assert/product-2.png"),
+      // productItem(context, "Smart", "900", "assert/product-3.png"),
+      // productItem(context, "Luggage", "100", "assert/product-4.png"),
+      // productItem(context, "Smartphone", "99", "assert/product-5.png"),
+      // productItem(context, "Speakers", "49", "assert/product-6.png"),
+      // productItem(context, "AirPods", "199", "assert/product-7.png"),
+    ],
+  );
+}
+
+Widget productItem() {
+  return Container(
+    height: 120,
+    margin: EdgeInsets.all(5),
+    child: Row(
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.all(10),
+          child: Image.asset(
+            "assets/product-1.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Título do produto"),
+              Text(
+                "\$200",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 30,
+                width: 120,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: FlatButton(
+                        child: Text("+"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: Text("1"),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: FlatButton(
+                        child: Text("-"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
